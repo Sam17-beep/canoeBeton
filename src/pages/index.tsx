aca88e1fb2eca395e01600aa0e36782425b30a20
Marc-Anthony Girard <magirard@nexapp.ca>
2022-12-05 14:26:36 -0500

    push page for old website

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
 import React from 'react'
+import ConstructionIcon from '@mui/icons-material/Construction';
+import { Icon } from '@mui/material';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <p>Hello world!</p>
-    </div>
+      <div style={{
+        display: 'flex', flexDirection: "column", alignItems: 'center', justifyContent: 'center', flex: '1 1 auto'}}>
+        <ConstructionIcon fontSize='large'></ConstructionIcon>
+        <p>Ce site est présentement en reconstruction. Veuillez utiliser le site suivant : <a href="https://canoeulaval.wixsite.com/site">https://canoeulaval.wixsite.com/site</a></p>
+      </div>
   )
 }
 
