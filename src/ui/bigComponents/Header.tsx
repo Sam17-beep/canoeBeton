04ea56b52db0793e8b878d7c784189eeedc2355b
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2022-12-25 13:26:49 -0500

    header space around

--- src/ui/bigComponents/Header.tsx
+++ src/ui/bigComponents/Header.tsx
@@ -1,35 +1,58 @@
-import Link from "next/link"
-import { FunctionComponent, PropsWithChildren } from "react"
+import Link from 'next/link';
+import { FunctionComponent, PropsWithChildren } from 'react';
 
 interface HeaderProps {
-  current: string
+  current: string;
 }
-const Header: FunctionComponent<PropsWithChildren<HeaderProps>> = ({ current }) => {
+const Header: FunctionComponent<PropsWithChildren<HeaderProps>> = ({
+  current,
+}) => {
   return (
     <div style={parent}>
-      <Link href="/" style={current === "home" ? activeLinkStyle : {}}>Accueil</Link>
-      <Link href="/event" style={current === "event" ? activeLinkStyle : {}}>Événement</Link>
-      <Link href="/team" style={current === "team" ? activeLinkStyle : {}}>Équipe</Link>
-      <Link href="/tournament" style={current === "tournament" ? activeLinkStyle : {}}>Historique</Link>
-      <Link href="/sponsor" style={current === "sponsor" ? activeLinkStyle : {}}>Partenaire</Link>
-      <Link href="/contact" style={current === "contact" ? activeLinkStyle : {}}>Nous joindre</Link>
+      <Link href='/' style={current === 'home' ? activeLinkStyle : {}}>
+        Accueil
+      </Link>
+      <Link href='/event' style={current === 'event' ? activeLinkStyle : {}}>
+        Événement
+      </Link>
+      <Link href='/team' style={current === 'team' ? activeLinkStyle : {}}>
+        Équipe
+      </Link>
+      <Link
+        href='/tournament'
+        style={current === 'tournament' ? activeLinkStyle : {}}
+      >
+        Historique
+      </Link>
+      <Link
+        href='/sponsor'
+        style={current === 'sponsor' ? activeLinkStyle : {}}
+      >
+        Partenaire
+      </Link>
+      <Link
+        href='/contact'
+        style={current === 'contact' ? activeLinkStyle : {}}
+      >
+        Nous joindre
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 const activeLinkStyle = {
-  color: "white",
-  backgroundColor: "blue",
-}
+  color: 'white',
+  backgroundColor: 'blue',
+};
 
 // css class for the parent div
 const parent = {
-  display: "flex",
-  justifyContent: "right",
-  alignItems: "center",
-  backgroundColor: "#333333",
-  width: "100%",
-  height: "40px",
-}
+  display: 'flex',
+  justifyContent: 'space-around',
+  alignItems: 'center',
+  backgroundColor: '#333333',
+  width: '100%',
+  height: '40px',
+};
 
 export default Header;
