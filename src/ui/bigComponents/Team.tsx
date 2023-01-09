5fa36dc96512d7efb1837a793a11899cbba4443e
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2023-01-08 20:29:30 -0500

    correction bug

--- src/ui/bigComponents/Team.tsx
+++ src/ui/bigComponents/Team.tsx
@@ -1,50 +1,53 @@
-import { FunctionComponent, PropsWithChildren, useEffect, useState } from 'react';
-import MemberCard from '../smallComponents/MemberCard';
-import styles from 'styles/ui/bigComponents/Team.module.css';
+import {
+  FunctionComponent,
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from 'react'
+import MemberCard from '../smallComponents/memberCard'
+import styles from 'styles/ui/bigComponents/Team.module.css'
 
 export interface Member {
-  name: string;
-  role: string;
-  description: string;
-  imagePath: string;
+  name: string
+  role: string
+  description: string
+  imagePath: string
 }
 
-
-
-const Team: FunctionComponent<PropsWithChildren> = ({  }) => {
-  const [members, setMembers] = useState<Member[]>([]);
+const Team: FunctionComponent<PropsWithChildren> = ({}) => {
+  const [members, setMembers] = useState<Member[]>([])
 
   useEffect(() => {
     async function fetchData() {
-      const res = await fetch('/api/team');
-      const data = await res.json();
-      setMembers(data);
+      const res = await fetch('/api/team')
+      const data = await res.json()
+      setMembers(data)
     }
-    fetchData();
-  }, []);
-  
+    fetchData()
+  }, [])
+
   return (
     <div className={styles.page}>
       <span className={styles.big_title}>Notre équipe</span>
       <span className={styles.title}>Nos responsable</span>
       <div className={styles.team_container}>
-        {members.map(member =>
+        {members.map((member) =>
           member.role !== "Membre de l'équipe" ? (
-            <MemberCard member={member} key={member.name} afficherRole/>
+            <MemberCard member={member} key={member.name} afficherRole />
           ) : null
         )}
       </div>
 
       <span className={styles.title}>Nos membres</span>
       <div className={styles.team_container}>
-        {members.map(member =>
+        {members.map((member) =>
           member.role === "Membre de l'équipe" ? (
             <MemberCard member={member} key={member.name} />
           ) : null
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Team;
+export default Team
