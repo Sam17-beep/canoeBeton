dcbcadf592095c719e05a6fa137d770e60f77015
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2023-01-10 15:59:04 -0500

    formattage i guess

--- src/ui/bigComponents/Accueil.tsx
+++ src/ui/bigComponents/Accueil.tsx
@@ -1,22 +1,19 @@
-import { FunctionComponent, PropsWithChildren } from "react"
-import Hero from "../smallComponents/Hero";
-import Information from "../smallComponents/Information";
-import Footer from "./Footer";
+import { FunctionComponent, PropsWithChildren } from 'react'
+import Hero from '../smallComponents/Hero'
+import Information from '../smallComponents/Information'
+import Footer from './Footer'
 import styles from 'styles/ui/bigComponents/Accueil.module.css'
 
-interface HeaderProps {
-}
+interface HeaderProps {}
 
-const Accueil: FunctionComponent<PropsWithChildren<HeaderProps>> = ({ }) => {
+const Accueil: FunctionComponent<PropsWithChildren<HeaderProps>> = ({}) => {
   return (
     <div className={styles.page}>
       <Hero />
       <Information />
-      <Footer/>
+      <Footer />
     </div>
   )
 }
 
-
-
-export default Accueil;
+export default Accueil
--- src/ui/smallComponents/hero.tsx
+++ src/ui/smallComponents/hero.tsx
@@ -1,47 +1,56 @@
 import Image from 'next/image'
-import { CSSProperties, FunctionComponent, PropsWithChildren } from "react"
+import { CSSProperties, FunctionComponent, PropsWithChildren } from 'react'
 
-interface HeaderProps {
-}
+interface HeaderProps {}
 
-const Hero: FunctionComponent<PropsWithChildren<HeaderProps>> = ({ }) => {
+const Hero: FunctionComponent<PropsWithChildren<HeaderProps>> = ({}) => {
   return (
-      <div style={parent}>
-        <Image src="/../public/Accueil/hero.webp" alt="Hero image" width={1920} height={1080}
-        style={{ maxWidth: '100%', height: 'auto', minHeight: "10em", objectPosition: "0 100"}} object-fit={"cover"} />
-        <div style={text}>
-          <span style={title}>Canoë de Béton</span>
-          <span style={subtitle}>DE L&apos;UNIVERSITÉ LAVAL</span>
-        </div>
+    <div style={parent}>
+      <Image
+        src="/../public/Accueil/hero.webp"
+        alt="Hero image"
+        width={1920}
+        height={1080}
+        style={{
+          maxWidth: '100%',
+          height: 'auto',
+          minHeight: '10em',
+          objectPosition: '0 100',
+        }}
+        object-fit={'cover'}
+      />
+      <div style={text}>
+        <span style={title}>Canoë de Béton</span>
+        <span style={subtitle}>DE L&apos;UNIVERSITÉ LAVAL</span>
       </div>
+    </div>
   )
 }
 
-const parent : CSSProperties | undefined  = {
-  flexDirection: "column",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  width: "100%",
+const parent: CSSProperties | undefined = {
+  flexDirection: 'column',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100%',
 }
 
 const text: CSSProperties | undefined = {
-  display: "flex",
-  flexDirection: "column",
-  position: "absolute",
-  top: "10em",
+  display: 'flex',
+  flexDirection: 'column',
+  position: 'absolute',
+  top: '10em',
 }
 
-const title : CSSProperties | undefined  = {
-  fontSize: "6em",
-  fontWeight: "bold",
-  color: "white",
+const title: CSSProperties | undefined = {
+  fontSize: '6em',
+  fontWeight: 'bold',
+  color: 'white',
 }
 
-const subtitle : CSSProperties | undefined = {
-  fontSize: "3em",
-  color: "white",
+const subtitle: CSSProperties | undefined = {
+  fontSize: '3em',
+  color: 'white',
 }
 
-
-export default Hero;
+export default Hero
